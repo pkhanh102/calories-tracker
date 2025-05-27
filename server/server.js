b9668e94bef96ac95af9eaf853fec1041e34c33d
pkhanh102 <phuckhanhng@gmail.com>
2025-05-27 20:53:05 +1000

    Add signup & login route with JWT authentication

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const authRoutes = require('./routes/auth');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors()); // Allow frontend to make requests
+app.use(express.json()); // Parse incoming JSON
+
+app.use('/api/auth', authRoutes); // Route group for /register and /login
+
+app.listen(PORT, () =>  {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
